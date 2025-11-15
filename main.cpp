1c75b993aa648acf0a4f10db062bb2e361571b3c
yul007 <158792261+yul007@users.noreply.github.com>
2025-11-15 15:11:23 -0500

    se cambio el main. Implement manual matrix input and algorithm options
    Se agrego el menu del main para escoger entre manual y aleatorio. Added functionality for manual matrix input and algorithm selection.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,66 @@
 //0.01 proyecto mates discretas
 
-#include<iostream>
-#include"local_include\Matrix.h"
-#include"local_include\YYC.h"
-#include"local_include\BT.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "local_include/Matrix.h"
+#include "local_include/YYC.h"
+#include "local_include/BT.h"
+#include "local_include/InputMatrix.h"
 
 int main()
 {
-    //init random
-    srand(time(0));
-    
+    cout << "¿Desea ingresar manualmente la Matriz de Diferencias? (1=si, 0=no): ";
+    int opcion = 0;
+    cin >> opcion;
+
+    if(opcion == 1) //INGRESA MATRIZ MANUAL
+    {
+        vector<vector<int>> M = LeerManualmenteMatrizDiferencia(); //Se ingresa MD manualmente
+        BooleanMatrix manualBM(M);
+
+        cout << "MD ingresada:\n";
+        PrintMatrix(manualBM.differenceMatrix);
+
+        cout << "MB calculada:\n";
+        PrintMatrix(manualBM.basicMatrix);
+
+        cout << "Density: " << manualBM.GetMatrixDensity() << endl;
+
+        // Escoger Algo
+            cout << "¿Que algoritmo deasea utilizar? (1=YYC, 0=BT): ";
+            int opcion2 = 0;
+            cin >> opcion2;
+
+            if (opcion2 == 1){
+                RunYYCIncremental(manualBM.basicMatrix);
+            } else if (opcion2 == 0){
+                    cout <<"Starting BT Algorithm..." << endl;
+                    BT bt(manualBM.basicMatrix.at(0).size());
+                
+                    cout << "Finding Typical Testors..." << endl;
+                    vector<vector<int>> typicalTestors = bt.FindTypicalTestors(manualBM.basicMatrix);
+                
+                    cout << "Typical Testors found:" << endl;
+                    
+                    PrintMatrix(typicalTestors);
+            }
+        return 0;
+    }
+
+
+    //MATRIZ ALEATORIA
+    srand(time(0));   //init random
     BooleanMatrix booleanMatrix(5, 5);
+    
     cout << "Difference Matrix: " << endl;
     PrintMatrix(booleanMatrix.differenceMatrix);
-
 
     cout << "Basic Matrix: " << endl;
     PrintMatrix(booleanMatrix.basicMatrix);
     cout << "Density: " << booleanMatrix.GetMatrixDensity() << endl;
 
+    
     cout << "Starting YYC Incremental Algorithm..." << endl;
     cout << "Finding Typical Testors..." << endl;
     RunYYCIncremental(booleanMatrix.basicMatrix);
